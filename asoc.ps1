0779d02c2f52b9cb46aacec25aef03bf8abf9dfb
Vishal-Chaudhary <102226698+vishalhcl-5960@users.noreply.github.com>
2024-09-04 23:27:19 +0530

    debugging

--- asoc.ps1
+++ asoc.ps1
@@ -671,7 +671,7 @@ function Create-EphemeralPresenceWithDocker{
       Write-Host "Checkpoint-4"
       Write-Host $env:GITHUB_ACTION_PATH/$dockerfileName
       Write-Host $dockerImageName
-  docker build -f $GITHUB_ACTION_PATH/$dockerfileName -t $dockerImageName .
+  docker build -f $env:GITHUB_ACTION_PATH/$dockerfileName -t $dockerImageName .
       Write-Host "Checkpoint-2"
   docker run --name $dockerContainerName -d $dockerImageName --debug
       Write-Host "Checkpoint-3"
