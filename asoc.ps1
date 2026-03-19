a86b291673bbeda11b84815a5cf4420ce724d000
Ankur Raina <ankur.raina@prod.hclpnp.com>
2026-03-19 16:19:46 +0530

    ASA-12585, Added support for AppScan 360 for running DAST scan.

--- asoc.ps1
+++ asoc.ps1
@@ -24,8 +24,8 @@ $ClientType = "github-dast-$Os-$env:GITHUB_ACTION_REF"
 # Handle SSL validation
 $global:SkipCert = $false
 
-if ($env:ACCEPTSSL -eq "false") {
-    Write-Host "ACCEPTSSL = false ? SSL validation will be ignored (Non-Prod AppScan 360)"
+if ($env:ACCEPTSSL -eq "true") {
+    Write-Host "ACCEPTSSL = true ? SSL validation will be ignored for AppScan 360"
     $global:SkipCert = $true
 }
 else {
